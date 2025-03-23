4895daa433c1b2bc580aa1d461c79c9782addb63
Priti kadam <pritikadam0308@gmail.com>
2025-03-23 22:21:42 +0530

    All images added

--- src/data/competitionData.js
+++ src/data/competitionData.js
@@ -18,7 +18,7 @@ export const competitions = [
       type: "Team",
       participants: 400,
       colleges: 22,
-      image: "/path/to/hackathon-image.jpg"
+      image: "https://raw.githubusercontent.com/Priti0308/pahal-public/main/IMG_8858.jpg"
     },
     {
       id: 3,
@@ -28,7 +28,7 @@ export const competitions = [
       type: "Individual",
       participants: 150,
       colleges: 10,
-      image: "/path/to/business-image.jpg"
+      image: "https://raw.githubusercontent.com/Priti0308/pahal-public/main/IMG_8851.jpg"
     },
     {
       id: 4,
@@ -38,7 +38,7 @@ export const competitions = [
       type: "Individual",
       participants: 600,
       colleges: 25,
-      image: "/path/to/sports-image.jpg"
+      image: "https://raw.githubusercontent.com/Priti0308/pahal-public/main/IMG_8872.jpg"
     },
     {
       id: 5,
@@ -58,7 +58,7 @@ export const competitions = [
       type: "Team",
       participants: 280,
       colleges: 12,
-      image: "/path/to/robotics-image.jpg"
+      image: "https://raw.githubusercontent.com/Priti0308/pahal-public/main/IMG_8883.jpg"
     },
     {
       id: 7,
@@ -68,7 +68,7 @@ export const competitions = [
       type: "Team",
       participants: 320,
       colleges: 20,
-      image: "/path/to/science-image.jpg"
+      image: "https://raw.githubusercontent.com/Priti0308/pahal-public/main/IMG_8896.jpg"
     },
     {
       id: 8,
@@ -78,7 +78,7 @@ export const competitions = [
       type: "Individual",
       participants: 180,
       colleges: 14,
-      image: "/path/to/chess-image.jpg"
+      image: "https://raw.githubusercontent.com/Priti0308/pahal-public/main/IMG_9049.jpg"
     },
     {
       id: 9,
@@ -88,7 +88,7 @@ export const competitions = [
       type: "Individual",
       participants: 220,
       colleges: 16,
-      image: "/path/to/music-image.jpg"
+      image: "https://raw.githubusercontent.com/Priti0308/pahal-public/main/IMG_9050.jpg"
     },
     {
       id: 10,
@@ -98,6 +98,6 @@ export const competitions = [
       type: "Team",
       participants: 260,
       colleges: 15,
-      image: "/path/to/startup-image.jpg"
+      image: "https://raw.githubusercontent.com/Priti0308/pahal-public/main/IMG_8990.jpg"
     }
   ];
