02fd93f06f4fc6dff5c96258fd12965957dcc182
Priti kadam <pritikadam0308@gmail.com>
2025-05-17 17:15:40 +0530

    Change the uin of manage events table

--- src/components/adminDashboard/ManageEvents.jsx
+++ src/components/adminDashboard/ManageEvents.jsx
@@ -73,7 +73,7 @@ const ManageEvents = () => {
   );
 
   return (
-    <div className="max-w-6xl mx-auto p-6">
+    <div className="max-w-9xl mx-auto p-6">
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-2xl font-bold text-gray-800">Manage Events</h1>
         <button
